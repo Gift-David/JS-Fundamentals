1dd75cb8bb2fc319fe9a5ed66c2d400ba834076a
David Gift <giftdavid189@gmail.com>
2025-11-09 23:20:21 +0100

    completed the 6th task

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,17 +1,17 @@
-let lists = [“C is fun”, “Python is cool”, “JavaScript is amazing”]
+let lists = ["C is fun", "Python is cool", "JavaScript is amazing"]
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+for (let index = 0; index < lists.length; index++) {
+    console.log(lists[index]);
     
 }
 
-Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+// Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 
-The first line: “C is fun”
-The second line: “Python is cool”
-The third line: “JavaScript is amazing”
-You must use console.log(...) to print all output
-You are not allowed to use var
-You are not allowed to use any if/else statement
-You can use only one console.log
-You must use a loop (while, for, etc.)
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+// You are not allowed to use any if/else statement
+// You can use only one console.log
+// You must use a loop (while, for, etc.)
