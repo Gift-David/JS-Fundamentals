8d2fc7e49926cfd78e84f20fc93c4f7f78f6c739
David Gift <giftdavid189@gmail.com>
2025-11-09 22:50:59 +0100

    completed task 3

---
+++ 3-value_argument.js
@@ -0,0 +1,18 @@
+const argv = process.argv.slice(2);
+
+if (argv == null){
+    console.log("No argument")
+}
+else {
+    console.log(argv[0])
+}
+
+
+
+
+// Write a script that prints the first argument passed to it:
+
+// If no arguments are passed to the script, print “No argument”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+// You are not allowed to use length
